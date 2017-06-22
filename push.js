e8b4ed5a6074a22c01bedee5a5a55d2eabf0d7dc
mitch lamers <mitchlamers@MacBook-Pro-van-mitch.local>
2017-06-22 22:17:07 +0200

    push file to repo

--- push.js
+++ push.js
@@ -1,8 +1,46 @@
-var simpleGit = require('simple-git');
+const simpleGit = require('simple-git');
+const fs = require('fs-extra')
+const rimraf = require('rimraf');
 
+const repo = `https://xx@bitbucket.org/user/repo.git`;
 const tempFolder = `./tmp`;
-const repo = `https://mitch_lamers@bitbucket.org/mitch_lamers/ftp.git`;
+const fileName = `example.js`;
+const newFile = `${tempFolder}/${fileName}`;
 
-simpleGit().clone(repo, tempFolder, (result) => {
-    console.log(result);
-})
+clone(repo);
+
+function clone(aRepo) {
+    rimraf(tempFolder, function () {
+        simpleGit().clone(repo, tempFolder, (result) => {
+            copyFile();
+        });
+    });
+}
+
+function copyFile() {
+    rimraf(newFile, function () {
+        fs.copy(`./${fileName}`, newFile)
+            .then(() => {
+                addToGit();
+            })
+            .catch(err => console.error(err))
+    });
+}
+
+function addToGit() {
+    const gitInDir = simpleGit('./tmp');
+
+    gitInDir.add(fileName, (result) => {
+        gitInDir.commit(`update ${fileName}`).push('origin', 'master', (succes) => {
+            removeFolder(tempFolder);
+        }, (error) => {
+            console.error('Commit or push failed');
+            removeFolder(tempFolder);
+        });
+    });
+}
+
+function removeFolder(aTempFolder) {
+    rimraf(aTempFolder, () => { });
+}
+
